757100123ecf9197b9ac73a365a13d9fcb255d9a
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-07-04 13:58:03 -0300

    add validation style

---
+++ web/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useForm } from 'react-hook-form'
+import './App.css';
+
+function App() {
+
+const { register, handleSubmit, errors} = useForm()
+  function onSubmit(data){
+    console.log(data)
+  }
+  return (
+    <div className="App">
+      <div className="box">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div >
+            <input name="user" ref={register({required:true})}/>
+            <label>User</label>
+          </div>
+          <div>
+            <input name="password" type="password" ref={register({required:true})}/>
+            <label>Password</label>
+          </div>
+          
+          {/* {errors.exampleRequire && <span>This field is required</span>} */}
+
+          <input type="submit"/>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
