256d2d01770b93f0d56c0f5840d5cf8173b4dfd5
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-08-15 15:12:19 -0300

    add middlewares in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,11 @@ import express from 'express'
 class App{
     constructor(){
         this.server = express();
+        this.middlewares()
+    }
+
+    middlewares(){
+        this.server.use(express.json())
     }
 }
 
